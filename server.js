8f0c28bc0d3c94335fbd85d00d74e5bfd8e1ae92
enricostano <ocirne.onats@gmail.com>
2015-09-02 14:56:18 +0200

    Add API server calls handling

--- server.js
+++ server.js
@@ -19,22 +19,54 @@ export default (initter, config) => {
 
   const appEnv = app.get('env');
 
+  const rails_url = 'localhost:3000'
+
   logger(app, appEnv, config.bundle);
 
   app.use(compressor());
-
   app.use(parser.json());
   app.use(parser.urlencoded({ extended: true }));
-
   app.use(cookies());
-
   app.use(express.static(path.join(__dirname, 'public')));
-  app.all('/api/*', function(req, res, next) {
-    // Verify authentication / cookies
+
+  app.post('/login', function (req, res) {
     // Call Rails
-    // Respond with Rails response
-    res.send('Welcome');
+    request.post(`http://${rails_url}/login`, function (err, response, body) {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.session.user_id = body;
+
+      res.sendStatus(200);
+    });
+  })
+
+  app.get('/api/*', function(req, res) {
+    // Verify authentication / cookies
+    if (!req.session.user_id) {
+      return res.sendStatus(403);
+    }
+
+    // Call Rails
+    var options = {
+      method: req.method,
+      url: `http://${rails_url}${req.path}`,
+      headers: {
+        'X-katuma-user-id': req.session.user_id
+      }
+    };
+
+    request(options, function (err, response, body) {
+      if (err) {
+        return res.sendStatus(400);
+      }
+
+      // Respond with Rails response
+      res.send(response.status, response.body);
+    });
   });
+
   app.use('/', noCache, initter);
 
   app.set('view engine', 'jade');
